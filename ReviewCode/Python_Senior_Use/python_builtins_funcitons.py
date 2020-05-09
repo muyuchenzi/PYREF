a3fd9cdf6a1b24fdc37b3d52cc4b26e96e038b95
muyuchenzi <muyuchenzi@gmail.com>
2020-05-09 18:04:32 +0800

    add-python  budiltins fuctions -normal

---
+++ ReviewCode/Python_Senior_Use/python_builtins_funcitons.py
@@ -0,0 +1,14 @@
+# abs
+
+class Buildin_funcitons(object):
+    def __init__(self,num_var,string_var,bool_var,dict_var,list_var,tuple_var):
+        self.num_var=num_var
+        self.string_var=string_var
+        self.bool_var=bool_var
+        self.dict_var=dict_var
+        self.list_var=list_var
+        self.tuple_var=tuple_var
+
+
+    def build_abs(self):
+        pass
