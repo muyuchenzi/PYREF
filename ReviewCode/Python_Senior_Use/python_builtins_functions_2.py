b62ad74ddc7ba5d48fb250350b712e7d3fa7d46d
muyuchenzi <muyuchenzi@gmail.com>
2020-05-12 17:51:37 +0800

    python buildin fuction done-noraml

--- ReviewCode/Python_Senior_Use/python_builtins_functions_2.py
+++ ReviewCode/Python_Senior_Use/python_builtins_functions_2.py
@@ -21,6 +21,41 @@ class Buildin_functions(object):
         filter_res = filter(lambda x: len(str(x)) > 3, self.list_var)
         print(list(filter_res))
 
+    def build_map(self):
+        map_result = map(lambda x: str(x) + "map_functions", self.list_var)
+        print(list(map_result))
+
+    def build_range(self):
+        range_res = range(len(self.list_var))
+        print(range_res)
+
+    def build_iter(self):
+        iter_res = iter(self.list_var)
+        print(next(iter_res))
+        print(next(iter_res))
+        print(next(iter_res))
+        print(next(iter_res))
+        print(next(iter_res))
+
+    def build_eval(self):
+        eval("print(f'this is heppened {self.num_var}')")
+        print("Done")
+
+    def build_exec(self):
+        exec("for i in range(10):"
+             "print(i)"
+             )
+
+
+global_string_vars = "string_var"
+global_num_vars = 23
+
+
+def func():
+    a = 10
+    print(locals())
+    # print(globals())
+
 
 if __name__ == '__main__':
     build_func = Buildin_functions(num_var=-23, string_var="December239%$@", bool_var=False,
@@ -31,3 +66,12 @@ if __name__ == '__main__':
     build_func.build_any()
     build_func.build_zip()
     build_func.build_filter()
+    build_func.build_map()
+    func()
+    print("-" * 20)
+    build_func.build_range()
+    build_func.build_iter()
+    build_func.build_eval()
+    build_func.build_exec()
+    print(callable(func))
+    print(callable(global_num_vars))
