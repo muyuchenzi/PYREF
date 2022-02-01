b1e3a5208d03c0bd317af31641b9b7618ea68bb7
muyuchenzi <muyuchenzi@gmail.com>
2022-02-02 00:33:25 +0800

    <ignore><Test python grammer><python>

--- ReviewCode/QA_for_InterView/Python_Basic/Test_Gram.py
+++ ReviewCode/QA_for_InterView/Python_Basic/Test_Gram.py
@@ -1,35 +1,13 @@
-import tkinter
-import tkinter.messagebox
+def read_file():
+    with open(r'E:\李震祥\PYGIT\PYref\ReviewCode\QA_for_InterView\Python_Basic\data\致橡树.txt', 'r',
+              encoding='utf-8') as f:
+        while True:
+            txt_line = f.readline()
+            if not txt_line:
+                break
+            yield txt_line
 
 
-def main():
-    flag = True
-
-    top = tkinter.Tk()
-    top.geometry('240x160')
-    top.title('小游戏')
-    label = tkinter.Label(top, text='hello,world', font='Arial -32', fg='red')
-    label.pack(expand=1)
-    panel = tkinter.Frame(top)
-
-    def change_label_text():
-        nonlocal flag
-        flag = not flag
-        colr, msg = ('red', 'hello,world') if flag else(
-            'blue', "goodbye,world")
-        label.config(text=msg, fg=colr)
-
-    def confim_to_quit():
-        if tkinter.messagebox.askokcancel('温馨提示', '确定要退出吗？'):
-            top.quit()
-
-    # 创建按钮对象 指定添加到哪个容器中 通过command参数绑定事件回调函数
-    button1 = tkinter.Button(panel, text='修改', command=change_label_text)
-    button1.pack(side='left')
-    button2 = tkinter.Button(panel, text='退出', command=confim_to_quit)
-    button2.pack(side='right')
-    panel.pack(side='bottom')
-
-    tkinter.mainloop()
-
-main()
+file_res = read_file()
+for line in file_res:
+    print(line)
