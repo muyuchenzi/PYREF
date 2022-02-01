b2e2b133e28cd2c4b0cfa29fd484e964244f696b
muyuchenzi <muyuchenzi@gmail.com>
2022-02-01 17:50:57 +0800

    ignore normal update

--- ReviewCode/QA_for_InterView/Python_Basic/Test_Gram.py
+++ ReviewCode/QA_for_InterView/Python_Basic/Test_Gram.py
@@ -1,26 +1,35 @@
-from timeit import timeit
+import tkinter
+import tkinter.messagebox
 
 
-def normal_gene_list():
-    normal_list = []
-    for i in range(1000):
-        normal_list.append(i)
-    return normal_list
+def main():
+    flag = True
 
+    top = tkinter.Tk()
+    top.geometry('240x160')
+    top.title('小游戏')
+    label = tkinter.Label(top, text='hello,world', font='Arial -32', fg='red')
+    label.pack(expand=1)
+    panel = tkinter.Frame(top)
 
-def generator_list(num):
-    for i in range(num):
-        yield i
+    def change_label_text():
+        nonlocal flag
+        flag = not flag
+        colr, msg = ('red', 'hello,world') if flag else(
+            'blue', "goodbye,world")
+        label.config(text=msg, fg=colr)
 
+    def confim_to_quit():
+        if tkinter.messagebox.askokcancel('温馨提示', '确定要退出吗？'):
+            top.quit()
 
-def get_gener_list(num):
-    # num = int(num)
-    generator_list_x = []
-    for x in generator_list(num):
-        generator_list_x.append(x)
-    return generator_list_x
+    # 创建按钮对象 指定添加到哪个容器中 通过command参数绑定事件回调函数
+    button1 = tkinter.Button(panel, text='修改', command=change_label_text)
+    button1.pack(side='left')
+    button2 = tkinter.Button(panel, text='退出', command=confim_to_quit)
+    button2.pack(side='right')
+    panel.pack(side='bottom')
 
+    tkinter.mainloop()
 
-print(timeit(normal_gene_list, number=3))
-res = timeit(lambda: get_gener_list(1000), number=1000)
-print(res)
+main()
