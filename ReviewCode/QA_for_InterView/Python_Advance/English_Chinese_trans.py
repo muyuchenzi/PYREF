a70ea1d2285c1bfd844a18507fc249ac3993c074
muyuchenzi <muyuchenzi@gmail.com>
2022-02-12 21:57:49 +0800

    <fixed>(local host update):修复了baidu 翻译的bug，换了另一种get方式

--- ReviewCode/QA_for_InterView/Python_Advance/English_Chinese_trans.py
+++ ReviewCode/QA_for_InterView/Python_Advance/English_Chinese_trans.py
@@ -3,7 +3,7 @@ import hashlib
 import urllib
 import random
 import json
-
+import requests
 def post_to_baidu(from_text,to_text,input_text):
     '''
     APP ID：20220212001080952
@@ -12,7 +12,7 @@ def post_to_baidu(from_text,to_text,input_text):
   
     '''
     appid="20220212001080952"
-    secretKey='7hmzzpojDNLIdUD5M'
+    secretKey='7hmzzpojDNLIdUD5MkUb'
     httpClient = None
     myurl = '/api/trans/vip/translate'
     fromLang=from_text
@@ -24,25 +24,44 @@ def post_to_baidu(from_text,to_text,input_text):
     #配置字段结束
     my_url=myurl+ '?appid=' + appid + '&q=' + urllib.parse.quote(q) + '&from=' +\
          fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+    # NOTE 第一种方法
+    trans_url='http://api.fanyi.baidu.com/api/trans/vip/translate'
+    params={
+        'q':input_text,
+        'from':from_text,
+        'to':to_text,
+        'appid':appid,
+        'salt':salt,
+        'sign':sign
+    }
     try:
-        httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
-        httpClient.request('Get', my_url)
-
-        # response是HTTPResponse对象
-        response = httpClient.getresponse()
-        result_all = response.read().decode("utf-8")
-        result = json.loads(result_all)
-
-        print (result)
-
+        response=requests.get(trans_url,params=params)
+        result_dict=response.json()
+        if 'trans_result' in result_dict:
+            return result_dict['trans_result'][0]['src'],result_dict['trans_result'][0]['dst']
+        else:
+            print('Some error occured: ',result_dict)
     except Exception as e:
-        print (e)
-    finally:
-        if httpClient:
-            httpClient.close()
+        print('访问失败！')
+#    NOTE 第二种方法 访问失败，不建议尝试。
+    # try:
+    #     httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+    #     httpClient.request('Get', my_url)
+
+    #     # response是HTTPResponse对象
+    #     response = httpClient.getresponse()
+    #     result_all = response.read().decode("utf-8")
+    #     result = json.loads(result_all)
+
+    #     print (result)
+
+    # except Exception as e:
+    #     print (e)
+    # finally:
+    #     if httpClient:
+    #         httpClient.close()
 
 def chinese_or_english(input_text):
-    input_text="test"
     from_text=''
     to_text=""
     for word in input_text:
@@ -58,8 +77,8 @@ def chinese_or_english(input_text):
 def func_entry():
     entry_text=input("请输入需要翻译的句子:")
     from_text,to_text=chinese_or_english(input_text=entry_text.strip())
-    post_to_baidu(from_text,to_text,entry_text)
-
+    from_result,to_result=post_to_baidu(from_text,to_text,entry_text)
+    print(from_result,to_result)
 
 
 if __name__=="__main__":
--- ReviewCode/QA_for_InterView/Python_Basic/temp.py
+++ ReviewCode/QA_for_InterView/Python_Basic/temp.py
@@ -1,48 +1,74 @@
-# coding=utf-8
-
-import http.client
 import hashlib
-import urllib
 import random
-import json
 import requests
-def test():
 
-    appid = '20220212001080952'  # 填写你的appid
-    secretKey = '7hmzzpojDNLIdUD5M'  # 填写你的密钥
 
-    httpClient = None
-    myurl = 'http://api/trans/vip/translate'
+# 你的APP ID
+appID = '20220212001080952'
+# 你的密钥
+secretKey = '7hmzzpojDNLIdUD5MkUb'
+# 百度翻译 API 的 HTTP 接口
+apiURL = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
 
-    fromLang = 'en'   #原文语种
-    toLang = 'zh'   #译文语种
-    salt = random.randint(32768, 65536)
-    q= 'test'
-    sign = appid + q + str(salt) + secretKey
-    sign = hashlib.md5(sign.encode()).hexdigest()
-    myurl =myurl + '?q=' + urllib.parse.quote(q) + '&from=' + fromLang + '&to=' + toLang +'&appid=' + appid+ '&salt=' + str(salt) + '&sign=' + sign
-    # text=requests.get(myurl)
-    # print(text)
+
+def baiduAPI_translate(query_str, to_lang):
+    '''
+    传入待翻译的字符串和目标语言类型，请求 apiURL，自动检测传入的语言类型获得翻译结果
+    :param query_str: 待翻译的字符串
+    :param to_lang: 目标语言类型
+    :return: 翻译结果字典
+    '''
+    # 生成随机的 salt 值
+    salt = str(random.randint(32768, 65536))
+    # 准备计算 sign 值需要的字符串
+    pre_sign = appID + query_str + salt + secretKey
+    # 计算 md5 生成 sign
+    sign = hashlib.md5(pre_sign.encode()).hexdigest()
+    # 请求 apiURL 所有需要的参数
+    params = {
+        'q': query_str,
+        'from': 'auto',
+        'to': to_lang,
+        'appid': appID,
+        'salt':salt,
+        'sign': sign
+    }
     try:
-        httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
-        httpClient.request('GET', myurl)
-
-        # response是HTTPResponse对象
-        response = httpClient.getresponse()
-        result_all = response.read().decode("utf-8")
-        result = json.loads(result_all)
-
-        print (result)
-
+        # 直接将 params 和 apiURL 一起传入 requests.get() 函数
+        response = requests.get(apiURL, params=params)
+        # 获取返回的 json 数据
+        result_dict = response.json()
+        # 得到的结果正常则 return
+        if 'trans_result' in result_dict:
+            return result_dict
+        else:
+            print('Some errors occured:\n', result_dict)
     except Exception as e:
-        print (e)
-    finally:
-        if httpClient:
-            httpClient.close()
-
-test()
+        print('Some errors occured: ', e)
 
 
-def temp():
+def baiduAPI_translate_main(query_str, dst_lang=''):
+    '''
+    解析翻译结果后输出，默认实现英汉互译
+    :param query_str: 待翻译的字符串，必填
+    :param dst_lang: 目标语言类型，可缺省
+    :return: 翻译后的字符串
+    '''
+    if dst_lang:
+        # 指定了目标语言类型，则直接翻译成指定语言
+        result_dict = baiduAPI_translate(query_str, dst_lang)
+    else:
+        # 未指定目标语言类型，则默认进行英汉互译
+        result_dict = baiduAPI_translate(query_str, 'zh')
+        if result_dict['from'] == 'zh':
+            result_dict = baiduAPI_translate(query_str, 'en')
+    # 提取翻译结果字符串，并输出返回
+    dst = result_dict['trans_result'][0]['dst']
+    print('{}: {} -> {}: {}'.format(result_dict['from'], query_str, result_dict['to'], dst))
+    return dst
 
-    ss=hashlib.md5()
+
+if __name__ == '__main__':
+    baiduAPI_translate_main('This is English.')
+    baiduAPI_translate_main('这是中文')
+    baiduAPI_translate_main('翻译成法语', 'fra')
