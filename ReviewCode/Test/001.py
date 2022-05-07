8c17aa6278248b6996f1ff34cefb91d11948a06e
muyuchenzi <muyuchenzi@gmail.com>
2022-05-07 21:53:30 +0800

    <ignore>(local host file name changed):review two self module

--- ReviewCode/Test/001.py
+++ ReviewCode/Test/001.py
@@ -2,16 +2,29 @@
 # 是解决问题的一个思路
 # while 循环特别注意不要死循环
 
-
-
 def func_alpha():
-    counter=20
+    counter = 20
     while counter:
-        print(counter)
-        counter-=1
+        print(counter, end=" ")
+        counter -= 1
     else:
         print("end")
-    
+
+
+def my_func():
+    a = [1, 2, 3]
+    for i in a:
+        if i == 2:
+            break
+        print(i)
+    for i in a:
+        if i == 2:
+            continue
+        print(i)
+
 
 if __name__ == "__main__":
     func_alpha()
+    my_func()
+
+    #
