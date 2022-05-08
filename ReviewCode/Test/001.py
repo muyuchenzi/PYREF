58b8eb80b9693d6a9327aa64eec0c1da22264bc8
muyuchenzi <muyuchenzi@gmail.com>
2022-05-08 09:50:11 +0800

    <ignore>(local host file name changed):review work two

--- ReviewCode/Test/001.py
+++ ReviewCode/Test/001.py
@@ -11,6 +11,7 @@ def func_alpha():
         print("end")
 
 
+#for 循环 中的break 与continue ，是跳出本次循环   ---跳出本次循环继续下次循环，这两者是有区别的。
 def my_func():
     a = [1, 2, 3]
     for i in a:
@@ -21,10 +22,14 @@ def my_func():
         if i == 2:
             continue
         print(i)
-
+    list_alpha=[1,2,3,4,5,6,7,8]
+    for i in range(0,len(list_alpha),2):
+        print(list_alpha[i],end=' ')
+    print(list_alpha[0:len(list_alpha):2])
 
 if __name__ == "__main__":
     func_alpha()
     my_func()
 
     #
+    
