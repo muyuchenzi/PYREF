bdf153f736106719c73e831ee16345309eb188fb
muyuchenzi <muyuchenzi@gmail.com>
2022-05-10 18:55:15 +0800

    <normal>(local host file name changed):闭包的一个测试,能够储存临时值

--- ReviewCode/Review_Work/Day11/advanced_01.py
+++ ReviewCode/Review_Work/Day11/advanced_01.py
@@ -43,3 +43,35 @@ gamma = alpha(5)
 # go(2)
 # go(3)
 # go(4)
+origin = 0
+
+
+# def go(step):
+#     global origin
+#     new_ops = origin+step
+#     origin = new_ops
+#     return new_ops
+
+# print(go(2))
+# print(go(3))
+# print(go(6))
+
+
+def travel(pos):
+    def day(step):
+        nonlocal pos
+        new_ops = pos+step
+        pos = new_ops
+        return new_ops
+    return day
+
+
+monday = travel(0)
+print(monday(4))
+print(monday(4))
+print(monday(4))
+
+wenday=travel(100)
+print(wenday(100))
+print(wenday(100))
+print(wenday(100))
