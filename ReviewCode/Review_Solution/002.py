4338aa6c4ff1d2b3c1c3250de1280ee3b45a7440
muyuchenzi <muyuchenzi@gmail.com>
2022-05-10 23:29:54 +0800

    <normal>(local host file name changed):闭包的一个测试,能够储存临时值

--- ReviewCode/Review_Solution/002.py
+++ ReviewCode/Review_Solution/002.py
@@ -1,39 +1,32 @@
-# 装饰器
-import time
+origin = 0
 
 
-def decorator(func):
-    def wrapper(number):
-        time_start = time.time()
-        reuslt=func(number)
-        time_end = time.time()
-        print(f"函数运行消耗了{time_end-time_start}s！,总数为：{reuslt}")
+# def go(step):
+#     global origin
+#     new_ops = origin+step
+#     origin = new_ops
+#     return new_ops
 
-    return wrapper
+# print(go(2))
+# print(go(3))
+# print(go(6))
 
 
+def travel(pos):
+    def day(step):
+        nonlocal pos
+        new_ops = pos+step
+        pos = new_ops
+        return new_ops
+    return day
 
-def isprime(num):
-    if num < 2:
-        return False
-    else:
-        for i in range(2, num):
-            if num % i == 0:
-                return False
-        return True
-@decorator
-def prime_nums(number):
-    # time_start=time.time()
-    count=0
-    for i in range(2,number):
-        if isprime(i):
-            count+=1
-            # time.sleep(1)
-            print(i)
-    return count
-    # time_end=time.time()
-    # print(f"总共花费了{time_end-time_start}s!")
 
-if __name__ == "__main__":
-    prime_nums(10000)
-    # print(isprime(34))
+monday = travel(0)
+print(monday(4))
+print(monday(4))
+print(monday(4))
+
+wenday=travel(100)
+print(wenday(100))
+print(wenday(100))
+print(wenday(100))
