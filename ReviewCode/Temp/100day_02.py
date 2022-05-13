794daa03fa736872beeed13e15feb77a45fa87bf
muyuchenzi <muyuchenzi@gmail.com>
2022-05-13 19:22:20 +0800

    <normal>(local host file name changed):gramma test

--- ReviewCode/Temp/100day_02.py
+++ ReviewCode/Temp/100day_02.py
@@ -1,3 +1,9 @@
-f=float(input("please input :"))
-c=(f-32)/1.8
-print(f"{c}为结果！")
+# 计算圆的半径周长面积
+
+input_alpha = input("请输入圆的半径")
+pi = 3.1415926
+
+permiter = 2*pi*float(input_alpha)
+area = pi*float(input_alpha)*float(input_alpha)
+print(f"圆的周长为：{permiter}")
+print(f"圆的面积为：{area}")
