f9f9948895f339a5b74325b6f001d6f9a46c3827
muyuchenzi <muyuchenzi@gmail.com>
2022-05-13 20:59:23 +0800

    <normal>(local host file name changed):gramma test

--- ReviewCode/Temp/100day_02.py
+++ ReviewCode/Temp/100day_02.py
@@ -1,9 +1,21 @@
-# 计算圆的半径周长面积
+# 猜数字游戏
+import random
 
-input_alpha = input("请输入圆的半径")
-pi = 3.1415926
 
-permiter = 2*pi*float(input_alpha)
-area = pi*float(input_alpha)*float(input_alpha)
-print(f"圆的周长为：{permiter}")
-print(f"圆的面积为：{area}")
+answer = random.randint(0, 100)
+
+round = 20
+count = 0
+print(answer)
+while True > 0:
+    input_answer = int(input("请输入0-100以内的数字(包括)："))
+    if input_answer > answer:
+        print("小一点")
+    elif input_answer < answer:
+        print("大一点")
+    else:
+        print("恭喜你猜对了")
+        break
+    round -= 1
+    count += 1
+print(f'你总共猜了{count}')
