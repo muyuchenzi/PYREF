4de2b7b3e903c847dcc4bd6329673fa9813cd0f5
muyuchenzi <muyuchenzi@gmail.com>
2022-05-14 17:05:13 +0800

    <normal>(local host file name changed):modify review

--- ReviewCode/QA_for_InterView/Python_Advance/English_Chinese_trans.py
+++ ReviewCode/QA_for_InterView/Python_Advance/English_Chinese_trans.py
@@ -4,82 +4,86 @@ import urllib
 import random
 import json
 import requests
-def post_to_baidu(from_text,to_text,input_text):
+
+
+def post_to_baidu(from_text, to_text, input_text):
     '''
     APP ID：20220212001080952
 
     密钥：7hmzzpojDNLIdUD5MkUb
   
     '''
-    appid="20220212001080952"
-    secretKey='7hmzzpojDNLIdUD5MkUb'
+    appid = "20220212001080952"
+    secretKey = '7hmzzpojDNLIdUD5MkUb'
     httpClient = None
     myurl = '/api/trans/vip/translate'
-    fromLang=from_text
-    toLang=to_text
-    salt=random.randint(32768, 65536)
-    q=input_text
-    sign=appid+q+str(salt)+secretKey
-    sign=hashlib.md5(sign.encode()).hexdigest()
-    #配置字段结束
-    my_url=myurl+ '?appid=' + appid + '&q=' + urllib.parse.quote(q) + '&from=' +\
-         fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+    fromLang = from_text
+    toLang = to_text
+    salt = random.randint(32768, 65536)
+    q = input_text
+    sign = appid + q + str(salt) + secretKey
+    sign = hashlib.md5(sign.encode()).hexdigest()
+    # 配置字段结束
+    my_url = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(q) + '&from=' + \
+             fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
     # NOTE 第一种方法
-    trans_url='http://api.fanyi.baidu.com/api/trans/vip/translate'
-    params={
-        'q':input_text,
-        'from':from_text,
-        'to':to_text,
-        'appid':appid,
-        'salt':salt,
-        'sign':sign
+    trans_url = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+    params = {
+        'q': input_text,
+        'from': from_text,
+        'to': to_text,
+        'appid': appid,
+        'salt': salt,
+        'sign': sign
     }
     try:
-        response=requests.get(trans_url,params=params)
-        result_dict=response.json()
+        response = requests.get(trans_url, params=params)
+        result_dict = response.json()
         if 'trans_result' in result_dict:
-            return result_dict['trans_result'][0]['src'],result_dict['trans_result'][0]['dst']
+            return result_dict['trans_result'][0]['src'], result_dict['trans_result'][0]['dst']
         else:
-            print('Some error occured: ',result_dict)
+            print('Some error occured: ', result_dict)
     except Exception as e:
         print('访问失败！')
+
+
 #    NOTE 第二种方法 访问失败，不建议尝试。
-    # try:
-    #     httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
-    #     httpClient.request('Get', my_url)
+# try:
+#     httpClient = http.client.HTTPConnection('api.fanyi.baidu.com')
+#     httpClient.request('Get', my_url)
 
-    #     # response是HTTPResponse对象
-    #     response = httpClient.getresponse()
-    #     result_all = response.read().decode("utf-8")
-    #     result = json.loads(result_all)
+#     # response是HTTPResponse对象
+#     response = httpClient.getresponse()
+#     result_all = response.read().decode("utf-8")
+#     result = json.loads(result_all)
 
-    #     print (result)
+#     print (result)
 
-    # except Exception as e:
-    #     print (e)
-    # finally:
-    #     if httpClient:
-    #         httpClient.close()
+# except Exception as e:
+#     print (e)
+# finally:
+#     if httpClient:
+#         httpClient.close()
 
 def chinese_or_english(input_text):
-    from_text=''
-    to_text=""
+    from_text = ''
+    to_text = ""
     for word in input_text:
-        if   '\u4e00'<=word<='\u9fff':
-            from_text='zh' 
-            to_text="en"
+        if '\u4e00' <= word <= '\u9fff':
+            from_text = 'zh'
+            to_text = "en"
         else:
-            from_text='en'
-            to_text="zh"
-    return from_text,to_text
+            from_text = 'en'
+            to_text = "zh"
+    return from_text, to_text
 
 
 def func_entry():
-    entry_text=input("请输入需要翻译的句子:")
-    from_text,to_text=chinese_or_english(input_text=entry_text.strip())
-    from_result,to_result=post_to_baidu(from_text,to_text,entry_text)
-    print(from_result,to_result)
+    entry_text = input("请输入需要翻译的句子:")
+    from_text, to_text = chinese_or_english(input_text=entry_text.strip())
+    from_result, to_result = post_to_baidu(from_text, to_text, entry_text)
+    print(from_result, to_result)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     func_entry()
--- ReviewCode/QA_for_InterView/Python_Basic/normal_algorithm.py
+++ ReviewCode/QA_for_InterView/Python_Basic/normal_algorithm.py
@@ -1,4 +1,3 @@
-
 # 一种常见的坐标移动问题，经常犯错，很简单的问题都找不到解决办法，这个最简单的问题给自己一个参考
 # '''
 # Question
@@ -18,25 +17,26 @@
 # Then, the output of the program should be:
 # 2'''
 import math
-positon=[0,0]
+
+positon = [0, 0]
 while True:
-    input_string=input()
+    input_string = input()
     if not input_string:
         break
-    movement=input_string.split(" ")
-    direction=movement[0]
-    steps=movement[1]
-    if direction=="UP":
-        positon[0]+=steps
-    elif direction=="DOWN":
-        positon[0]-=steps
-    elif direction=="LEFT":
-        positon[1]-=steps
-    elif direction=="RIGHT":
-        positon[1]+=steps
+    movement = input_string.split(" ")
+    direction = movement[0]
+    steps = movement[1]
+    if direction == "UP":
+        positon[0] += steps
+    elif direction == "DOWN":
+        positon[0] -= steps
+    elif direction == "LEFT":
+        positon[1] -= steps
+    elif direction == "RIGHT":
+        positon[1] += steps
     else:
         pass
-result=int(round(math.sqrt(positon[0]**2+positon[1]**2)))
+result = int(round(math.sqrt(positon[0] ** 2 + positon[1] ** 2)))
 '''
 二叉树查找元素
 Please write a binary search function which searches an item 
@@ -45,19 +45,21 @@ in a sorted list. The function should return the index of element to be searched
 
 import math
 
-def bin_search(li,element):
-    bottom=0
-    top=len(li)
-    index=-1
-    while top>=bottom and index==-1:
-        mid=int(math.floor((top+bottom)/2.0))
-        if li[mid]==element:
-            index=mid
-        elif li[mid]>element:
-             top=mid-1
+
+def bin_search(li, element):
+    bottom = 0
+    top = len(li)
+    index = -1
+    while top >= bottom and index == -1:
+        mid = int(math.floor((top + bottom) / 2.0))
+        if li[mid] == element:
+            index = mid
+        elif li[mid] > element:
+            top = mid - 1
         else:
-            bottom=mid+1
+            bottom = mid + 1
     return index
 
-li=[i for i in range(10000)]
-print(bin_search(li,7788))
+
+li = [i for i in range(10000)]
+print(bin_search(li, 7788))
