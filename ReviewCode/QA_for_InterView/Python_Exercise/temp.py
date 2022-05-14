fd514be814b39f311306e32f438dc75dd2701a16
muyuchenzi <muyuchenzi@gmail.com>
2022-05-14 22:27:11 +0800

    <normal>(local host file name changed):modify review

--- ReviewCode/QA_for_InterView/Python_Exercise/temp.py
+++ ReviewCode/QA_for_InterView/Python_Exercise/temp.py
@@ -1,12 +1,45 @@
-import random
-print([i*11 for i in range(10)])
-list_alpha=[1,2,3,4,5]
-list_beta=[3,4,5,6,7,3,4,5]
-set_alpha=set(list_alpha)
-set_beta=set(list_beta)
+import os
+from functools import reduce
+class Solution(object):
+    
+    @staticmethod
+    def reverse(x):
+        if -10<x<10:
+            return x
+        str_x=str(x)
+        if str_x[0]!='-':
+            res=int(str_x[::-1])
+        else:
+            str_x=str_x[1:][::-1]
+            res=-int(str_x)
+        return res if -2147483648<x<2147483647 else 0
 
-list_gamma=['b','c','a','d','c','a']
-list_delta=list_gamma.copy()
+    @staticmethod
+    def get_pyfiles(dir_path,suffix):
+        res=[]
+        for root,dir,file_name in os.walk(dir_path):
+            res.append(file_name)
+        
+        file_list=[]
+        for r in res:
+            for i in r:
+                file_list.append(i)
+        
+        final_res=[]
+        for file in file_list:
+            if file.endswith(suffix):
+                final_res.append(file)
+        print(final_res)
+    @staticmethod
+    def sum(num_list):
+        print(sum(num_list))
+        print(reduce(lambda x,y :x+y,num_list))
+if __name__=="__main__":
+    s=Solution()
+    result=s.reverse(-120)
+    print(result)
+    dir_path=r'E:\李震祥\PYGIT\PYref\ReviewCode\QA_for_InterView\Python_Exercise'
+    s.get_pyfiles(dir_path,'.py')
+    s.sum([i for i in range(200)])
 
-xx=list(set(list_beta))
-xx.sort(key=xx.index)
+            
