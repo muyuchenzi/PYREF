f25f2c9f15cd75ebce42ebe07db8a8ce404e40a4
muyuchenzi <muyuchenzi@gmail.com>
2022-05-15 17:49:48 +0800

    <normal>(local host file name changed):modify review

--- ReviewCode/QA_for_InterView/Python_Exercise/temp.py
+++ ReviewCode/QA_for_InterView/Python_Exercise/temp.py
@@ -2,57 +2,28 @@ import os
 from functools import reduce
 from string import ascii_lowercase
 from string import digits
+
+
 class Solution(object):
     
-    @staticmethod
-    def reverse(x):
-        if -10<x<10:
-            return x
-        str_x=str(x)
-        if str_x[0]!='-':
-            res=int(str_x[::-1])
-        else:
-            str_x=str_x[1:][::-1]
-            res=-int(str_x)
-        return res if -2147483648<x<2147483647 else 0
-
-    @staticmethod
-    def get_pyfiles(dir_path,suffix):
-        res=[]
-        for root,dir,file_name in os.walk(dir_path):
-            res.append(file_name)
-        
-        file_list=[]
-        for r in res:
-            for i in r:
-                file_list.append(i)
-        
-        final_res=[]
-        for file in file_list:
-            if file.endswith(suffix):
-                final_res.append(file)
-        print(final_res)
-    @staticmethod
-    def sum(num_list):
-        print(sum(num_list))
-        print(reduce(lambda x,y :x+y,num_list))
-
-    @staticmethod
-    def get_missing_letter(a):
-        s1=set(ascii_lowercase)
-        s2=set(a)
-        print("------")
-        print("".join(sorted(list(s1-s2))))
-        
+    def temp(slef,str_data):
+        dict_str={}
+        for i in str_data:
+            dict_str[i]=dict_str.get(i,0)+1
+        return dict_str
     
-        
-if __name__=="__main__":
-    s=Solution()
-    result=s.reverse(-120)
-    print(result)
-    dir_path=r'E:\李震祥\PYGIT\PYref\ReviewCode\QA_for_InterView\Python_Exercise'
-    s.get_pyfiles(dir_path,'.py')
-    s.sum([i for i in range(200)])
-    s.get_missing_letter("python")
+    
+    def main_in(self):
+        dict_str=self.temp("AABBCCCADDDDDDDDDDD")
+        str_count_data=""
+        for k,v in dict_str.items():
+            str_count_data+=k+str(v)
+        print(str_count_data)
 
-            
+
+        
+        
+if __name__ == "__main__":
+    s = Solution()
+    s.main_in()
+    
