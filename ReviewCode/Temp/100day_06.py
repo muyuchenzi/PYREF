da95099b627fd7892dcfa67575abee6729943689
muyuchenzi <muyuchenzi@gmail.com>
2022-05-17 00:46:27 +0800

    <normal><python>(local host file name changed):modify review

--- ReviewCode/Temp/100day_06.py
+++ ReviewCode/Temp/100day_06.py
@@ -1,6 +1,6 @@
 # 求阶C(M,N
 import random
-
+from functools import reduce
 
 def add(*args):
     total = 0
@@ -10,3 +10,12 @@ def add(*args):
     return total
 
 print(add(1,2))
+
+
+def temp(m,n):
+    res_m=reduce(lambda x,y:x*y,m)
+    res_n=reduce(lambda x,y:x+y,n)
+    print(res_m)
+    print(res_n)
+
+temp([i for i in range(10)],[i for i in range(20)])
