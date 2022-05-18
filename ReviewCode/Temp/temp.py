c480555781edd5be88173e2c7de0e96388605a4f
muyuchenzi <muyuchenzi@gmail.com>
2022-05-18 22:16:09 +0800

    <normal><python>(local host file name changed):modify review

---
+++ ReviewCode/Temp/temp.py
@@ -0,0 +1,14 @@
+# 1、让代码可以被导入也可以被执行
+# if __name__ =="__main__":
+
+# 逻辑真或者假的
+# if x: if not x:
+
+# name = 'jackfrued'
+# fruits = ['apple', 'orange', 'grape']
+# owners = {'1001': '骆昊', '1002': '王大锤'}
+# if name and fruits and owners:
+#     print('I love fruits!')
+
+# 变量交换
+# a,b=b,a
