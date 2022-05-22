346d28eb21a90cfec0e2e93cceb2ff1821fddf8f
muyuchenzi <muyuchenzi@gmail.com>
2022-05-22 21:55:07 +0800

    <fix><python>(local host file name changed):pandas 编程练习100例

--- ReviewCode/pandas/temp.py
+++ ReviewCode/pandas/temp.py
@@ -2,4 +2,9 @@ import pandas as pd
 import numpy as np
 
 np.random.seed(80)
-df = pd.DataFrame(np.random.rand(10, 4), columns=list("ABCD"))
+df = pd.DataFrame(np.random.rand(4, 4), columns=list("ABCD"))
+
+# 61
+
+df['E'] = (df['C'] * df["D"] > 0.1) * 1
+df["F"] = sum(df['E']) / len(df)
