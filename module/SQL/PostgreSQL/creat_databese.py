dd1146635916c76a9a1c3042b9bd8c85f28ea2a0
muyuchenzi <muyuchenzi@gmail.com>
2022-05-24 00:53:43 +0800

    <normal><python>(local host file name changed):ready for SQL

---
+++ module/SQL/PostgreSQL/creat_databese.py
@@ -0,0 +1,8 @@
+import psycopg2
+from sqlalchemy import create_engine
+
+cnn = psycopg2.connect(host='127.0.0.1', port=5432, dbname='muyuchenzi',
+                       user='postgres', password="lizhenxiang")
+
+curs = cnn.cursor()
+engin = create_engine("postgresql://postgre@127.0.0.1:5432/muyuchenzi")
