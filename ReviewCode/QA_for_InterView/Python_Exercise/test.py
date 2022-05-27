eabd4d5ade470f2027fcbf808137785ab972f377
muyuchenzi <muyuchenzi@gmail.com>
2022-05-27 21:29:40 +0800

    <normal><Python>(local host file name changed):一个多重继承的经典例子

--- ReviewCode/QA_for_InterView/Python_Exercise/test.py
+++ ReviewCode/QA_for_InterView/Python_Exercise/test.py
@@ -1,16 +1,11 @@
-import pandas as pd
-import time
-import os
-import datetime
-import random
 from string import ascii_lowercase
-from functools import reduce
-from collections import Counter
-import re
-import random
+
+'''
+多重继承的问题，这个是一个模板例子
+'''
 
 
-class A(object):
+class A():
     class_a = [i for i in range(100)]
 
     def __init__(self, a, *args, **kwargs):
@@ -18,6 +13,8 @@ class A(object):
         获取class_a的前多个元素的和
         :param a:
         '''
+        super(A, self).__init__(*args, **kwargs)
+        # super().__init__(*args,**kwargs) 这个也可以
         self.a = a
 
     def print_a(self):
@@ -27,21 +24,23 @@ class A(object):
         '''
         print(self.a)
 
-    def cal_sum(self, number):
+    def show_sum(self, number):
         result = sum(self.class_a[0:number])
         return result
 
 
-class B(object):
+class B():
     class_b = ascii_lowercase
 
     def __init__(self, b, *args, **kwargs):
         '''
         获取calss_b前多个元素
         '''
+        super(B, self).__init__(*args, **kwargs)
+        # super().__init__(*args,**kwargs) 这个也可以的
         self.b = b
 
-    def pri_b(self):
+    def print_b(self):
         print(self.b)
 
     def show_str(self, number):
@@ -56,9 +55,10 @@ class C(A, B):
 
     def __init__(self, a, b, c):
         super(C, self).__init__(a, b)
+        # super().__init__(a,b)这个可以的，在多父类进行初始化的时候直接用super 就好，
         self.c = c
 
-    def pri_c(self):
+    def print_c(self):
         print(self.c)
 
     def show_dict(self, key):
@@ -66,9 +66,34 @@ class C(A, B):
         return result
 
 
-def answer():
-    ...
+class D(C):
+    class_d = set(ascii_lowercase)
+
+    def __init__(self, a, b, c, d):
+        super().__init__(a, b, c)
+        self.d = d
+
+    def print_d(self):
+        print(self.d)
 
 
 if __name__ == "__main__":
-    answer()
+    chenzi = C(5, 4, 5)
+    chenzi.print_a()
+    chenzi.print_b()
+    chenzi.print_c()
+
+    class_a_vari = chenzi.class_a
+    class_b_vari = chenzi.class_b
+    class_c_vari = chenzi.class_c
+    print(f"class_a_vari:{class_a_vari},\nclass_b_vari:{class_b_vari},\nclass_c_vari:{class_c_vari}")
+
+    res_sum = chenzi.show_sum(4)
+    res_str = chenzi.show_str(4)
+    res_dic = chenzi.show_dict('e')
+    print(f"class_a sum :{res_sum},\nclass_b str{res_str},\n class_c dict {res_dic}")
+
+    muyu = D(6, 7, 8, 9)
+    muyu.print_d()
+    muyu.print_a()
+    ss = muyu.show_str(5)
