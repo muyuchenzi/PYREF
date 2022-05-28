7b7adf41090f3a128109db2579ec5638a342b19d
muyuchenzi <muyuchenzi@gmail.com>
2022-05-29 00:00:47 +0800

    <ignore><Python>(local host file name changed):None

--- ReviewCode/QA_for_InterView/Python_Exercise/temp.py
+++ ReviewCode/QA_for_InterView/Python_Exercise/temp.py
@@ -5,28 +5,41 @@ import datetime
 import random
 from string import ascii_lowercase
 from functools import reduce
+import re
 
 
-class Foo():
+def answer():
     '''
-    可变类型与不可变类型：
-    可变类型：list dict set
-    不可变类型 ： string int tuple
+    re模块的两种匹配模式，一种是贪婪模式一种是非贪婪模式
+    1、字符
+    \w 字符 \W
+    \d 数字 \D
+    \s space\S
+    数量词：
+    ？
+    +
+    *
+    {m,n}
+    组
+    []
+    ()
+    ^开头
+    $结尾
+    函数：
+    最常用 findall 查找并以list返回
+    sub 将匹配到的字符进行替换
+    match 从开头第一个字符进行匹配
+    search 查找一遍，有group函数
 
     '''
-    pass
 
 
-def entrance():
-    str_alpha = "123"
-    list_alpha = [1, 2, 3]
-
-    print(list_alpha.index(3))
-    # result=0
-    # for i in str_alpha:
-    #     result+=int(i)*10
-    # print(result)
+def func(str_in):
+    temp = str_in.group()
+    dict_alpha = dict(zip([i for i in 'abc'], [i for i in range(10)]))
+    print(dict_alpha.get(temp, '0'))
+    return str(dict_alpha.get(temp, '0'))
 
 
 if __name__ == "__main__":
-    file_contents = entrance()
+    answer()
