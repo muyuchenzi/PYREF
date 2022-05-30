029431ef01342e46f8641d339f8e30c6918bbba8
muyuchenzi <muyuchenzi@gmail.com>
2022-05-30 23:54:17 +0800

    <ignore><Python>(local host file name changed):临时

--- ReviewCode/QA_for_InterView/Python_Exercise/temp.py
+++ ReviewCode/QA_for_InterView/Python_Exercise/temp.py
@@ -1,88 +1,13 @@
-import pandas as pd
-import time
-import os
-import datetime
 import random
-from string import ascii_lowercase
-from functools import reduce
-import re
-from threading import Thread
-from multiprocessing import Process
-from concurrent.futures import ThreadPoolExecutor
-from concurrent.futures import ProcessPoolExecutor
-from queue import Queue
-from threading import Lock
+import numpy as np
 
-
-def time_spend(func):
-    def wrapper(*args, **kwargs):
-        time_start = time.perf_counter()
-        func(*args, **kwargs)
-        time_end = time.perf_counter()
-        print(f"-----------> 总共消耗时间:{time_end - time_start} <-----------")
-
-    return wrapper
-
-
-def day_week_up(parameter=None):
-    print('-------week up start---------\n')
-    print(parameter)
-
-    week_up_list = [_ for _ in 'weekup']
-    for i in week_up_list:
-        time.sleep(0.5)
-        print(i)
-    print('----------week up end--------\n')
-
-
-def day_code(parameter=None):
-    print('----------code start---------\n')
-    print(parameter)
-    code_list = [i for i in 'iscode']
-    result = []
-    for i in code_list:
-        result.append(i)
-        time.sleep(0.5)
-        print(i)
-    print('----------code end-----------\n')
-
-
-def day_sleep(parameter=None):
-    print('----------sleep start--------\n')
-    print(parameter)
-    sleep_list = [i for i in 'asleep']
-    for _ in sleep_list:
-        time.sleep(0.5)
-        print(_)
-    print('----------sleep end----------\n')
-
-
-def day_think(para, q):
-    print('----------think start----------\n')
-    print(para)
-    temp_res = []
-    while True:
-        if not q.empty():
-            temp_res.append(q.get())
-        else:
-            break
-    result = '-'.join(temp_res)
-    print(result)
-    print('----------think end----------\n')
-
-
-@time_spend
-def answer_thread_queue():
-    '''
-    '''
-    q = Queue()
-    for i in 'muyuchenzi':
-        q.put(i)
-    t = Thread(target=day_think, args=('xx', q), name='week_up')
-    t.start()
-    t.join()
-
-
-if __name__ == "__main__":
-    # answer_thread()
-    answer_thread_queue()
+prices = {
+    'AAPL': 191.88,
+    'GOOG': 1186.96,
+    'IBM': 149.24,
+    'ORCL': 48.44,
+    'ACN': 166.89,
+    'FB': 208.09,
+    'SYMC': 21.29
+}
+res = sorted(prices.items(), key=lambda x: x[1], reverse=True)
