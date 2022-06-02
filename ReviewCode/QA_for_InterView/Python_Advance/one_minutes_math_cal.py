5c60d1b327da90671f4e1c63f726fca2a32f1c94
muyuchenzi <muyuchenzi@gmail.com>
2022-06-02 11:54:27 +0800

    <ignore><Python>(local host update):no sense

--- ReviewCode/QA_for_InterView/Python_Advance/one_minutes_math_cal.py
+++ ReviewCode/QA_for_InterView/Python_Advance/one_minutes_math_cal.py
@@ -6,7 +6,6 @@
 - 1分钟时间结束，显示总题数和正确率（正确率精确到小数点后2位），并将之前的题目和答案显示出来
 """
 
-
 import time
 import random
 
@@ -16,10 +15,10 @@ def get_divisor(n):
     出题：加减乘除运算，拿到除数的分母，
     '''
     res = []
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         if n % i == 0:
             res.append(i)
-    result=random.choice(res)
+    result = random.choice(res)
     return result
 
 
@@ -34,23 +33,23 @@ def get_varies(operator):
 
 def get_answer(a, b, operator):
     if operator == "+":
-        return a+b
+        return a + b
     elif operator == "-":
-        return a-b
+        return a - b
     elif operator == "x":
-        return a*b
-    elif operator =="÷":
-        return a/b
+        return a * b
+    elif operator == "÷":
+        return a / b
 
 
 def func_main():
     time_start = time.time()
     operators = ["+", "-", "x", "÷"]
-    questions=[]
-    answers=[]
-    input_answers=[]
-    correct=0
-    while time.time()-time_start < 60:
+    questions = []
+    answers = []
+    input_answers = []
+    correct = 0
+    while time.time() - time_start < 60:
         operator = random.choice(operators)
         # operator="÷"
         x, y = get_varies(operator)
@@ -60,16 +59,16 @@ def func_main():
         print(f"Question: {x} {operator} {y},请输入结果：")
         input_answer = input()
         input_answers.append(int(input_answer))
-        if int(input_answer)==answer:
+        if int(input_answer) == answer:
             print("回答正确，请继续...")
-            correct+=1
+            correct += 1
         else:
             print("回答错误，请继续...")
-     
-    print("总共{}道题目，回答正确为{}，准确率为{:.2f}%".format(len(questions),correct,
-    correct/len(questions)*100))
+
+    print("总共{}道题目，回答正确为{}，准确率为{:.2f}%".format(len(questions), correct,
+                                               correct / len(questions) * 100))
     print(questions)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     func_main()
--- module/SKLearn/temp.py
+++ module/SKLearn/temp.py
@@ -1,17 +1,32 @@
 # Q&A
 import numpy as np
 import pandas as pd
-
+import random
 # 1、构建一个dataFrame，输出每列缺失值的bili
+import datetime
+import string
+import time
+from functools import reduce, partial
+import copy
+import os
 
-a = "string_list"
+a = 1
 
 
-def temp():
-    a = {"a": 1, "b": 3}
-    b = {'c': 3, 'd': 4}
-    res = list(a.items()) + list(b.items())
-    dict(res)
+def temp(*args, **kwargs):
+    '''
+    下划线，_一个下划线就是一般是指私有变量
+    __两个是指内置函数
+    函数式编程 lambda、reduce、filter、map
+    :return:
+    '''
+    f = [i for i in range(10)]
+    t = []
+    for i in f.copy():
+        t.append(i)
+        f.remove(i)
+    print(f)
+    print(t)
 
 
 if __name__ == '__main__':
