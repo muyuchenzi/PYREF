db980a01ff135059cf30fd5ce5aa3f615089105f
muyuchenzi <muyuchenzi@gmail.com>
2022-10-24 16:58:19 +0800

    <ignore><Python>(local host code update):nothing

--- ReviewCode/QA_for_InterView/Multi_process_thread/advace_thread.py
+++ ReviewCode/QA_for_InterView/Multi_process_thread/advace_thread.py
@@ -7,6 +7,8 @@ import random
 '''
 #NOTE使用queue来进行使用多线程，多进程
 '''
+
+
 @blog_data.time_spend
 def multi_thread_craw():
     threads = []
@@ -21,10 +23,12 @@ def multi_thread_craw():
 
 def multi_thread_queue_craw(url_queue, html_queue):
     while True:
-        url = url_queue.get(blog_data.urls)
+        url = url_queue.get()
+        # url = url_queue.get(blog_data.urls)
         html = blog_data.craw_data(url)
         html_queue.put(html)
-        print(threading.current_thread().name, f"craw:{url}", f"url_queue.size:", url_queue.qsize())
+        print(threading.current_thread().name,
+              f"craw:{url}", f"url_queue.size:", url_queue.qsize())
         time.sleep(random.randint(1, 2))
 
 
@@ -32,7 +36,8 @@ def multi_thread_queue_prase(html_queue, file_save):
     while True:
         html = html_queue.get()
         results = blog_data.parse_data(html)
-        print(threading.current_thread().name, f"parse:{len(results)}", f"html_queue.size:", html_queue.qsize())
+        print(threading.current_thread().name,
+              f"parse:{len(results)}", f"html_queue.size:", html_queue.qsize())
         # print(results)
         for result in results:
             file_save.write(str(result) + '\n')
@@ -51,7 +56,8 @@ def run():
         # craw_threads.append(t)
         t.start()
     # parse_threads = []
-    file_save = open(r"E:\李震祥\PYGIT\PYref\ReviewCode\QA_for_InterView\Multi_process_thread\craw_data.txt", "w")
+    file_save = open(
+        r"E:\李震祥\PYGIT\PYref\ReviewCode\QA_for_InterView\Multi_process_thread\craw_data2.txt", "w")
     for i in range(5):
         t = threading.Thread(target=multi_thread_queue_prase, args=(html_queue, file_save),
                              name=f"parese{i}")
